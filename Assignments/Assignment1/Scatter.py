6c3146b2ab4ffe6cd2f05183a2d665f9d29dfeda
Max Magnusson <maxerik453@gmail.com>
2023-01-20 11:49:28 +0100

    scatterplot start

--- Assignments/Assignment1/Scatter.py
+++ Assignments/Assignment1/Scatter.py
@@ -1,3 +1,26 @@
-import pandas
+import pandas as pd
 import matplotlib.pyplot as plt
+
+def init():
+    csvfile1 = "Assignments\Assignment1\Data\gdp-per-capita-in-us-dollar-world-bank.csv"
+    csvfile2 = "Assignments\Assignment1\Data\life-expectancy.csv"
+    return csvfile1, csvfile2
+
+
+def scatterPlot(csvfile1, csvfile2):
+    dfGDP = pd.read_csv(csvfile1)
+    dfLife = pd.read_csv(csvfile2)
+
+
+
+    for col in dfGDPgrowth.columns:
+        plt.scatter(dfGDPgrowth[col], dfpopulation[col], label= "GDP per capita vs. Life expectancy Worldwise 2019")
+    plt.legend(loc='best', fontsize=16)
+    plt.xlabel('Life expectancy')
+    plt.ylabel('GDP per capita')
+
+
+
+csvfile1, csvfile2 = init()
+scatterPlot(csvfile1, csvfile2)
 
