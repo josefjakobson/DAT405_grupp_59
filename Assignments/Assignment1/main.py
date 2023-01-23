3e662fe387c984ee13a28d9534d0e4615ec48105
josefjakobson <j.jakobson28@gmail.com>
2023-01-23 16:40:57 +0100

    equalized and trimmet national gdp

--- Assignments/Assignment1/main.py
+++ Assignments/Assignment1/main.py
@@ -4,10 +4,13 @@ import trim_csv_data
 
 gdp_csv = "Assignments\Assignment1\Data\gdp-per-capita-in-us-dollar-world-bank.csv"
 life_expectancy_csv = "Assignments\Assignment1\Data\life-expectancy.csv"
+gdp_total_csv = "Assignments\Assignment1\Data\\national-gdp.csv"
 merged = "Assignments\Assignment1\Data\merged_data.csv"
 
 def start():
+    trim_csv_data.main(gdp_total_csv)
     merge.init(gdp_csv, life_expectancy_csv)
+    trim_csv_data.equalize(merged, gdp_total_csv)
     Scatter.init(merged)
 
 start()
