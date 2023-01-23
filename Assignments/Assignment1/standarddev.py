eee68b6386ee5b96dc3f8728fe887b4377c11fc2
josefjakobson <j.jakobson28@gmail.com>
2023-01-23 16:29:17 +0100

    finished standarddev

--- Assignments/Assignment1/standarddev.py
+++ Assignments/Assignment1/standarddev.py
@@ -1,6 +1,8 @@
 import pandas as pd
-
 data = pd.read_csv("Assignments\Assignment1\Data\merged_data.csv")
 sd = data["Life_expectancy"].std()
 mean = data["Life_expectancy"].mean()
-
+for elem in data.loc[data['Life_expectancy'] > mean + sd]["Entity"]:
+    # locates all entries where life expectancy was higher than one 
+    #standard deviation above the mean
+    print(elem)
