d3e1f4223b3b8bd8580ff1595c03eb6f1c890edc
josefjakobson <j.jakobson28@gmail.com>
2023-01-23 14:14:23 +0100

    Finished scatter and saved image

--- Assignments/Assignment1/Scatter.py
+++ Assignments/Assignment1/Scatter.py
@@ -2,25 +2,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def init():
-    csvfile1 = "Assignments\Assignment1\Data\gdp-per-capita-in-us-dollar-world-bank.csv"
-    csvfile2 = "Assignments\Assignment1\Data\life-expectancy.csv"
-    return csvfile1, csvfile2
+    csvfile = "Assignments\Assignment1\Data\merged_data.csv"
+    return csvfile
 
 
-def scatterPlot(csvfile1, csvfile2):
-    dfGDP = pd.read_csv(csvfile1)
-    dfLifeExpectancy = pd.read_csv(csvfile2)
+def scatterPlot(csvfile):
+    data = pd.read_csv(csvfile)
+    
+    plt.scatter(data["GDP per capita (constant 2015 US$)"], data["Life_expectancy"], marker=".")
+    plt.xlabel("GPD per Capita in USD")
+    plt.ylabel("Life Expectancy in years")
+    plt.title("Life Expectancy vs GPD per capita in 2019")
+    plt.show()
 
 
-
-    for col in dfGDP.columns:
-        plt.scatter(dfGDP[col], dfLifeExpectancy[col], label= "GDP per capita vs. Life expectancy Worldwise 2019")
-    plt.legend(loc='best', fontsize=16)
-    plt.xlabel('Life expectancy')
-    plt.ylabel('GDP per capita')
-
-
-
-csvfile1, csvfile2 = init()
-scatterPlot(csvfile1, csvfile2)
+csvfile = init()
+scatterPlot(csvfile)
 
