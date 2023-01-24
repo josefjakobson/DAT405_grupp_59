c29b0daaae8e7aa4d40f2dc5b19b2c874a49bb10
Max Magnusson <maxerik453@gmail.com>
2023-01-24 14:20:15 +0100

    highlow, added gdp per capita

--- Assignments/Assignment1/highlow.py
+++ Assignments/Assignment1/highlow.py
@@ -3,22 +3,31 @@ import pandas as pd
 data = pd.read_csv("Assignments\Assignment1\Data\merged_data.csv")
 mean_Life = data["Life_expectancy"].mean()
 mean_GDP = data["GDP (output, multiple price benchmarks)"].mean()
-condition_high_gpd = data["GDP (output, multiple price benchmarks)"] > mean_GDP
-condition_high_life = data['Life_expectancy'] > mean_Life
-condition_low_gpd = data["GDP (output, multiple price benchmarks)"] < mean_GDP
+mean_GDP_per_capita = data["GDP per capita (constant 2015 US$)"].mean()
+high_gdp = data["GDP (output, multiple price benchmarks)"] > mean_GDP
+high_life = data['Life_expectancy'] > mean_Life
+low_gpd = data["GDP (output, multiple price benchmarks)"] < mean_GDP
+high_gdp_per_capita = data["GDP per capita (constant 2015 US$)"] > mean_GDP_per_capita
+
+
+
+
+#here are all conditions. they all have high life expectancy
+low_gdp_condition = high_life & low_gpd
+high_gdp_condition = high_life & high_gdp
+high_gdp_per_capita_condition = high_life & high_gdp_per_capita
 
 def get_countries(condition):
     resulted_Countries = data.loc[condition]
     for country in resulted_Countries["Entity"]:
-        # locates all entries where life expectancy was higher than the mean and
-        # GDP lower than the mean
+        # locates all entries for a certain condition
         print(country)
 
 print("High Life, Low GDP")
-high_life_and_low_gdp = condition_high_life & condition_low_gpd
-get_countries(high_life_and_low_gdp)
+get_countries(low_gdp_condition)
 
 print("High Life, High GDP")
-high_life_and_high_gdp = condition_high_life & condition_high_gpd
-get_countries(high_life_and_high_gdp)
+get_countries(high_gdp_condition)
 
+print("High Life, High GDP per capita")
+get_countries(high_gdp_per_capita)
