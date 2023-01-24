7cdc2f43aee998bef61baa202385acb48920f0aa
josefjakobson <j.jakobson28@gmail.com>
2023-01-24 14:03:54 +0100

    fixed high-low

--- Assignments/Assignment1/highlow.py
+++ Assignments/Assignment1/highlow.py
@@ -1,14 +1,17 @@
 import pandas as pd
 
-data_Life = pd.read_csv("Assignments\Assignment1\Data\merged_data.csv")
-data_GDP = pd.read_csv("Assignments\Assignment1\Data\\national-gdp.csv")
-mean_Life = data_Life["Life_expectancy"].mean()
-mean_GDP = data_GDP["GDP (output, multiple price benchmarks)"].mean()
-
-for elem in data_Life.loc[data_Life ["Life_expectancy"] > mean_Life]:
-    if data_GDP.loc[data_GDP ["GDP (output, multiple price benchmarks)"] > mean_GDP]:
-# locates all entries where life expectancy was higher than the mean and
-# GDP lower than the mean
-        print(elem)
+data = pd.read_csv("Assignments\Assignment1\Data\merged_data.csv")
+mean_Life = data["Life_expectancy"].mean()
+mean_GDP = data["GDP (output, multiple price benchmarks)"].mean()
+condition_low_life = data['Life_expectancy'] < mean_Life
+condition_high_gpd = data["GDP (output, multiple price benchmarks)"] > mean_GDP
+condition_high_life = data['Life_expectancy'] > mean_Life
+condition_low_gpd = data["GDP (output, multiple price benchmarks)"] < mean_GDP
 
 
+combined = condition_low_life & condition_high_gpd
+resulted_Countries = data.loc[combined]
+for country in resulted_Countries["Entity"]:
+        # locates all entries where life expectancy was higher than the mean and
+        # GDP lower than the mean
+        print(country)
