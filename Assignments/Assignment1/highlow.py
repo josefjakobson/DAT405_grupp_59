8e041d4cce72e31cc212705eaddf0d5cd234497a
josefjakobson <j.jakobson28@gmail.com>
2023-01-24 14:58:37 +0100

    more refactoring

--- Assignments/Assignment1/highlow.py
+++ Assignments/Assignment1/highlow.py
@@ -6,6 +6,7 @@ mean_GDP = data["GDP (output, multiple price benchmarks)"].mean()
 mean_GDP_per_capita = data["GDP per capita (constant 2015 US$)"].mean()
 high_gdp = data["GDP (output, multiple price benchmarks)"] > mean_GDP
 high_life = data['Life_expectancy'] > mean_Life
+low_life = data['Life_expectancy'] < mean_Life
 low_gpd = data["GDP (output, multiple price benchmarks)"] < mean_GDP
 high_gdp_per_capita = data["GDP per capita (constant 2015 US$)"] > mean_GDP_per_capita
 
@@ -16,6 +17,9 @@ high_gdp_per_capita = data["GDP per capita (constant 2015 US$)"] > mean_GDP_per_
 low_gdp_condition = high_life & low_gpd
 high_gdp_condition = high_life & high_gdp
 high_gdp_per_capita_condition = high_life & high_gdp_per_capita
+low_life_High_GPD_condition = low_life & high_gdp
+low_life_High_GPD_per_capita_condition = low_life & high_gdp_per_capita
+
 
 def get_countries(condition):
     resulted_Countries = data.loc[condition]
@@ -23,11 +27,9 @@ def get_countries(condition):
         # locates all entries for a certain condition
         print(country)
 
-print("High Life, Low GDP")
-get_countries(low_gdp_condition)
 
-print("High Life, High GDP")
-get_countries(high_gdp_condition)
+for condition in [low_gdp_condition, high_gdp_condition, high_gdp_per_capita_condition, low_life_High_GPD_condition, low_life_High_GPD_per_capita_condition]:
+    print("\n")
+    get_countries(condition)
+    print("\n") 
 
-print("High Life, High GDP per capita")
-get_countries(high_gdp_per_capita)
