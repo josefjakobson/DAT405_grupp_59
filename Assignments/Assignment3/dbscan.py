b70df1970a02b5861515dba40cda664c289176c4
josefjakobson <j.jakobson28@gmail.com>
2023-02-06 20:21:21 +0100

    finished scatterplot

--- Assignments/Assignment3/dbscan.py
+++ Assignments/Assignment3/dbscan.py
@@ -1,45 +1,19 @@
 from sklearn.cluster import DBSCAN
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+from collections import Counter
+
 
 data = pd.read_csv("Assignments\Assignment3\Data\data_assignment3.csv")
 
 data = pd.DataFrame(data, columns=["phi", "psi"])
+clusters = DBSCAN(eps=15, min_samples=50).fit(data)
+labels = clusters.labels_
+p = sns.scatterplot(data=data, x="phi", y="psi", hue=labels, palette="deep", legend=False)
 
-clustering = DBSCAN(eps=3, min_samples=2).fit(data)
-labels = clustering.labels_
+outliers = Counter(labels)[-1] #Calculate number of outliers
 
-unique_labels = set(labels)
-core_samples_mask = np.zeros_like(labels, dtype=bool)
-core_samples_mask[clustering.core_sample_indices_] = True
-
-colors = [plt.cm.Spectral(each) for each in np.linspace(0, 1, len(unique_labels))]
-for k, col in zip(unique_labels, colors):
-    if k == -1:
-        # Black used for noise.
-        col = [0, 0, 0, 1]
-
-    class_member_mask = labels == k
-
-    xy = data[class_member_mask & core_samples_mask]
-    plt.plot(
-        xy[:, 0],
-        xy[:, 1],
-        "o",
-        markerfacecolor=tuple(col),
-        markeredgecolor="k",
-        markersize=14,
-    )
-
-    xy = data[class_member_mask & ~core_samples_mask]
-    plt.plot(
-        xy[:, 0],
-        xy[:, 1],
-        "o",
-        markerfacecolor=tuple(col),
-        markeredgecolor="k",
-        markersize=6,
-    )
+print(outliers)
 
 plt.show()
