5ac33768869200f0330f29caf8f674f15722100c
josefjakobson <j.jakobson28@gmail.com>
2023-02-06 13:29:31 +0100

    started assignment

---
+++ Assignments/Assignment3/Scatter.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+data = pd.read_csv("Assignments\Assignment3\Data\data_assignment3.csv")
+    
+sns.scatterplot(data=data, x="phi", y="psi", hue="residue name")
+    
+plt.show()
+
+
+
---
+++ Assignments/Assignment3/histogram.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+data = pd.read_csv("Assignments\Assignment3\Data\data_assignment3.csv")
+    
+sns.histplot(data=data, x="phi", y="psi")
+    
+plt.show()
+
