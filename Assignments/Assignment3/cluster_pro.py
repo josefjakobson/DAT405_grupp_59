b6c66470f14204b8bc73c52ac089326952c47b8c
josefjakobson <j.jakobson28@gmail.com>
2023-02-07 13:38:38 +0100

    cluster pro

--- Assignments/Assignment3/cluster_pro.py
+++ Assignments/Assignment3/cluster_pro.py
@@ -7,17 +7,10 @@ from collections import Counter
 
 data = pd.read_csv("Assignments\Assignment3\Data\data_assignment3.csv")
 
-data = pd.DataFrame(data, columns=["phi", "psi"])
-clusters = DBSCAN(eps=15, min_samples=50).fit(data)
+pro_data = data[data["residue name"] == "PRO"]
+
+data = pd.DataFrame(pro_data, columns=["phi", "psi"])
+clusters = DBSCAN(eps=10, min_samples=4).fit(data)
 labels = clusters.labels_
 sns.scatterplot(data=data, x="phi", y="psi", hue=labels, palette="deep", legend=False)
-counter = dict(Counter(labels))
-outliers = counter[-1] #Calculate number of outliers
-
-print(outliers)
-'''
-plt.bar(list(counter.keys()),counter.values())
-plt.xlabel("Cluster label")
-plt.ylabel("Occurrence")
-'''
 plt.show()
